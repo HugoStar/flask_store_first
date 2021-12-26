42da11ad8dedc5ae69d1e56656355de5a70784fe
Hugo <mrgyugo@gmail.com>
2021-12-27 00:29:42 +0300

    add authorization

--- code/app.py
+++ code/app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request
 
+from flask_jwt import JWT, jwt_required
+
 from flask_restful import Api, Resource
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
 app.secret_key = 'hugo'
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': f'An item wit name {name} already exists'}, 400
@@ -27,6 +34,7 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_required()
     def get(self):
         return {'items': items}
 
--- code/security.py
+++ code/security.py
@@ -6,16 +6,16 @@ users = [
     User(1, 'bob', 'asdf'),
 ]
 
-username_mapping = {u.username: u for u in users}
-userid_maping = {u.id: u for u in users}
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user = userid_maping.get(username, None)
+    user = username_table.get(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_maping.get(user_id, None)
+    return userid_table.get(user_id, None)
