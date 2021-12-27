3b8468b4f5d4c9bd13f5f6491d20da26ec48ff05
Hugo <mrgyugo@gmail.com>
2021-12-28 00:07:28 +0300

    delete item

--- code/app.py
+++ code/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-from flask_jwt import JWT, jwt_required
+from flask_jwt import JWT
 
 from flask_restful import Api, Resource
 
@@ -18,12 +18,10 @@ items = []
 
 
 class Item(Resource):
-    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
-    @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': f'An item wit name {name} already exists'}, 400
@@ -34,9 +32,13 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}, 201
+
 
 class ItemList(Resource):
-    @jwt_required()
     def get(self):
         return {'items': items}
 
