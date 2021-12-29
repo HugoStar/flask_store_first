254459778a2084919154c9e720eae3ccf90fbfa9
Hugo <mrgyugo@gmail.com>
2021-12-29 16:11:29 +0300

    work with SQLIte

--- code/app.py
+++ code/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 from flask_jwt import JWT
 
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 from security import authenticate, identity
 
@@ -18,24 +18,46 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field connot be left blank!'
+    )
+
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+    
+        data = Item.parser.parse_args()
+    
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': f'An item wit name {name} already exists'}, 400
-        data = request.get_json()
+        
         item = {'name': name,
                 'price': data['price'],
                 }
+                
         items.append(item)
+        
         return item, 201
 
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}, 201
+        
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
